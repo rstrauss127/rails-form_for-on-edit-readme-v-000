7b97920846a6c775e7843fd419484ac3d0558ad5
Rebecca Strauss <rstrauss127@gmail.com>
2019-04-02 20:55:21 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :new, :create, :show, :edit]
-patch 'posts/:id', to: 'posts#update'
+  resources :posts, only: [:index, :new, :create, :show, :edit, :update]
+
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
